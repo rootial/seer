0e8f264c9624ec052f76124ffa2d69fe8da6f9a6
Mingliang Liu <mingliang.liu@airbnb.com>
2020-07-10 01:06:02 +0800

    Updates

---
+++ precache-manifest.37d8b22cf30abdd466ed9d54cb01fb89.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bc65ee6618fc8d4f575fdbec4863d95",
+    "url": "/seer/index.html"
+  },
+  {
+    "revision": "e784b7a376414ef4656f",
+    "url": "/seer/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f82f1fdf12c13abc8fa5",
+    "url": "/seer/static/js/2.7b3995c1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/seer/static/js/2.7b3995c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e784b7a376414ef4656f",
+    "url": "/seer/static/js/main.c71b5cfd.chunk.js"
+  },
+  {
+    "revision": "2597ed06921f59f2a5af",
+    "url": "/seer/static/js/runtime-main.d015728f.js"
+  },
+  {
+    "revision": "a8dff6706db2b3165892c2a9ebcfc53e",
+    "url": "/seer/static/media/Pic1.a8dff670.png"
+  },
+  {
+    "revision": "bdc73b9eb85975666621574055828995",
+    "url": "/seer/static/media/Pic2.bdc73b9e.png"
+  },
+  {
+    "revision": "19b87882cf80a9cb971e47171a7e45da",
+    "url": "/seer/static/media/Pic3.19b87882.png"
+  },
+  {
+    "revision": "0e30ff725a37f5b11e72bf84311802a3",
+    "url": "/seer/static/media/Pic4.0e30ff72.png"
+  },
+  {
+    "revision": "2a29d8b225c854090fded908b16ff2a7",
+    "url": "/seer/static/media/video_poster.2a29d8b2.png"
+  }
+]);
